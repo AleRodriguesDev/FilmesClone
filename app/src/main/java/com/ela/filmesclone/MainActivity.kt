314d074b7de7b6856c696d87e5e920aedeecae6f
Ale TI <alessandrodev@outlook.com>
2023-08-30 09:20:26 -0300

    Initial commit

---
+++ app/src/main/java/com/ela/filmesclone/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.ela.filmesclone
+
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.ela.filmesclone.view.FormLogin
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        window.statusBarColor = Color.parseColor("#000000")
+
+        Handler(Looper.getMainLooper()).postDelayed({
+         val intent = Intent(this,FormLogin::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
---
+++ app/src/main/java/com/ela/filmesclone/view/FormLogin.kt
@@ -0,0 +1,12 @@
+package com.ela.filmesclone.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ela.filmesclone.R
+
+class FormLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_login)
+    }
+}
