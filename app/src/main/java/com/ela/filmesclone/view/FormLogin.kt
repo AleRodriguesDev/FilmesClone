a07d217e6ee4a8a605d30c802a523e5ab4abab81
Ale TI <alessandrodev@outlook.com>
2023-10-23 12:46:28 -0300

    commit login f

--- app/src/main/java/com/ela/filmesclone/view/FormLogin.kt
+++ app/src/main/java/com/ela/filmesclone/view/FormLogin.kt
@@ -1,14 +1,40 @@
 package com.ela.filmesclone.view
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ela.filmesclone.R
+import com.ela.filmesclone.databinding.ActivityFormLoginBinding
 
 class FormLogin : AppCompatActivity() {
+    private lateinit var binding: ActivityFormLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_form_login)
+        binding = ActivityFormLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         window.statusBarColor = Color.parseColor("#000000")
+        //binding.editEmail.requestFocus()
+
+        binding.btEntrar.setOnClickListener(){
+            val email = binding.editEmail.text.toString()
+            val senha = binding.editSenha.text.toString()
+
+            when {
+                email.isEmpty() -> {
+                    binding.containerEmail.helperText = "Preencha o email"
+                    binding.containerEmail.boxStrokeColor = Color.parseColor("#FF8F00")
+                }
+                senha.isEmpty() -> {
+                    binding.containerSenha.helperText = "Preencha a senha"
+                    binding.containerSenha.boxStrokeColor = Color.parseColor("#FF8F00")
+                }
+            }
+        }
+
+        binding.txtTelaCadastro.setOnClickListener(){
+            val intent = Intent(this,FormCadastro::class.java)
+            startActivity(intent)
+        }
     }
 }
